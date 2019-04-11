d1784be13b8b92853c10b9ff1923d0217d292180
zoey <zuyicai@umich.edu>
2019-04-10 22:24:55 -0400

    add original files to repo

---
+++ models.py
@@ -0,0 +1,35 @@
+Park
+Topic
+state
+
+
+activity
+
+
+
+class National_Park():
+    name
+    Location
+    description
+
+    def fname(arg):
+        pass
+    def fname(arg):
+        pass
+
+
+class Hostorical_Park(National_Park):
+    def fname(arg):
+        pass
+
+
+class National_Monument(National_Park):
+
+
+class National_Military_Park(National_Park):
+
+
+class National_Preserve(National_Park):
+
+
+class National_Heritage_Area(National_Park):
